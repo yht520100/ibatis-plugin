3f3ca97f24bc39473e8c0445c4a0bf67c5671b2f
libing.chen <libing.chen@a27988d2-9830-0410-8773-31d6671a21de>
2008-07-01 06:57:40 +0000

    minor update

--- src/org/intellij/ibatis/dom/configuration/TypeHandler.java
+++ src/org/intellij/ibatis/dom/configuration/TypeHandler.java
@@ -2,6 +2,7 @@ package org.intellij.ibatis.dom.configuration;
 
 import com.intellij.javaee.model.xml.CommonDomModelElement;
 import com.intellij.psi.PsiClass;
+import com.intellij.util.xml.Attribute;
 import com.intellij.util.xml.GenericAttributeValue;
 import org.jetbrains.annotations.NotNull;
 
@@ -11,12 +12,15 @@ import org.jetbrains.annotations.NotNull;
  * @author linux_china@hotmail.com
  */
 public interface TypeHandler extends CommonDomModelElement {
+    @Attribute("javaType")
     @NotNull
     public GenericAttributeValue<String> getJavaType();
 
+    @Attribute("jdbcType")
     @NotNull
     public GenericAttributeValue<String> getJdbcType();
 
+    @Attribute("callback")
     @NotNull
     public GenericAttributeValue<PsiClass> getCallback();
 }
