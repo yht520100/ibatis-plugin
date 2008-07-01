2d5576cde91dc1aa591a87fc9f0769dc579b88ac
libing.chen <libing.chen@a27988d2-9830-0410-8773-31d6671a21de>
2008-07-01 05:36:29 +0000

    typeHandler element

---
+++ src/org/intellij/ibatis/dom/configuration/impl/TypeHandlerImpl.java
@@ -0,0 +1,12 @@
+package org.intellij.ibatis.dom.configuration.impl;
+
+import org.intellij.ibatis.dom.configuration.TypeHandler;
+import com.intellij.javaee.model.xml.impl.BaseImpl;
+
+/**
+ * type handler implement
+ *
+ * @author linux_china@hotmail.com
+ */
+public abstract class TypeHandlerImpl extends BaseImpl implements TypeHandler {
+}
