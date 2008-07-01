493c8793fb887e1f79b5577e676baab34883eb3c
libing.chen <libing.chen@a27988d2-9830-0410-8773-31d6671a21de>
2008-07-01 05:50:25 +0000

    add getAllTypeHandlers method

--- src/org/intellij/ibatis/impl/IbatisManagerImpl.java
+++ src/org/intellij/ibatis/impl/IbatisManagerImpl.java
@@ -2,6 +2,7 @@ package org.intellij.ibatis.impl;
 
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtil;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.xml.XmlTag;
@@ -11,6 +12,7 @@ import org.intellij.ibatis.IbatisConfigurationModel;
 import org.intellij.ibatis.IbatisManager;
 import org.intellij.ibatis.IbatisProjectComponent;
 import org.intellij.ibatis.IbatisSqlMapModel;
+import org.intellij.ibatis.dom.configuration.TypeHandler;
 import org.intellij.ibatis.dom.sqlMap.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -26,7 +28,8 @@ public class IbatisManagerImpl extends IbatisManager {
     public IbatisManagerImpl() {
     }
 
-    @Nullable public IbatisConfigurationModel getConfigurationModel(@NotNull Module module) {
+    @Nullable
+    public IbatisConfigurationModel getConfigurationModel(@NotNull Module module) {
         IbatisProjectComponent projectComponent = IbatisProjectComponent.getInstance(module.getProject());
         List<IbatisConfigurationModel> models = projectComponent.getConfigurationModelFactory().getAllModels(module);
         if (models.size() > 0) return models.get(0);
@@ -55,7 +58,8 @@ public class IbatisManagerImpl extends IbatisManager {
     }
 
 
-    @Nullable public IbatisSqlMapModel getSqlMapModel(@Nullable PsiElement psiElement) {
+    @Nullable
+    public IbatisSqlMapModel getSqlMapModel(@Nullable PsiElement psiElement) {
         if (psiElement == null) return null;
         IbatisProjectComponent projectComponent = IbatisProjectComponent.getInstance(psiElement.getProject());
         return projectComponent.getSqlMapModelFactory().getModel(psiElement);
@@ -80,6 +84,28 @@ public class IbatisManagerImpl extends IbatisManager {
             allAliasMap.putAll(sqlMapModel.getTypeAlias());
         }
         return allAliasMap;
+    }
+
+    /**
+     * get all type handler in iBATIS
+     *
+     * @return type handler map
+     */
+    public Map<String, TypeHandler> getAllTypeHandlers(PsiElement psiElement) {
+        Map<String, TypeHandler> typeHandlerMap = new HashMap<String, TypeHandler>();
+        Module module = ModuleUtil.findModuleForPsiElement(psiElement);
+        IbatisProjectComponent projectComponent = IbatisProjectComponent.getInstance(module.getProject());
+        List<IbatisConfigurationModel> configurationModels = projectComponent.getConfigurationModelFactory().getAllModels(module);
+        for (IbatisConfigurationModel configurationModel : configurationModels) {
+            List<TypeHandler> typeHandlerList = configurationModel.getMergedModel().getTypeHandlers();
+            for (TypeHandler typeHandler : typeHandlerList) {
+                String javaType = typeHandler.getJavaType().getStringValue();
+                if (StringUtil.isNotEmpty(javaType)) {
+                    typeHandlerMap.put(javaType, typeHandler);
+                }
+            }
+        }
+        return typeHandlerMap;
     }
 
     /**
