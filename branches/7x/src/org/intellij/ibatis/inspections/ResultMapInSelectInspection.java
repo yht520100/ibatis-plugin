8d8f11707e3c4e087d44ee57980fef4b1af9fee3
libing.chen <libing.chen@a27988d2-9830-0410-8773-31d6671a21de>
2009-02-24 01:41:10 +0000

    minor update

--- branches/7x/src/org/intellij/ibatis/inspections/ResultMapInSelectInspection.java
+++ branches/7x/src/org/intellij/ibatis/inspections/ResultMapInSelectInspection.java
@@ -65,7 +65,7 @@ public class ResultMapInSelectInspection extends SqlMapInspection {
                             }
                         }
                     }
-                } catch (Exception e) {
+                } catch (Throwable e) {
                         
                 }
             }
